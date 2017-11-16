eb4e50b0e5ab8ebe30099bbe64af44562f0e0a7a
shangyang <comedshang@163.com>
2017-11-16 14:34:31 +0800

    stream pipeline

--- pipeline/src/test/java/org/shangyang/basic/stream/StreamTest.java
+++ pipeline/src/test/java/org/shangyang/basic/stream/StreamTest.java
@@ -3,6 +3,7 @@ package org.shangyang.basic.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -21,6 +22,19 @@ public class StreamTest {
 		
 		System.out.println( longestStringLengthStartingWithA.isPresent() ? longestStringLengthStartingWithA.getAsInt() : "null" );
 		
+	}
+	
+	@Test
+	public void testStream2(){
+		
+		List<String> strings = Arrays.asList("abc", "bdfafda", "ab", "abdace");
+		
+	    List<String> sorted  = strings.stream()
+	              .filter(s -> s.startsWith("a"))	              
+	              .sorted()
+	              .collect(Collectors.toList());
+	    
+	    System.out.println( sorted.toString() );
 	}
 	
 	
