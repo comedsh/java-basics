1edb0ec3369930588df261e4f5f19bdf2646c8ee
shangyang <comedshang@163.com>
2017-11-16 17:34:05 +0800

    stream pipeline

--- pipeline/src/test/java/org/shangyang/basic/stream/StreamTest.java
+++ pipeline/src/test/java/org/shangyang/basic/stream/StreamTest.java
@@ -1,9 +1,14 @@
 package org.shangyang.basic.stream;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalInt;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -35,7 +40,21 @@ public class StreamTest {
 	              .collect(Collectors.toList());
 	    
 	    System.out.println( sorted.toString() );
+	    
 	}
 	
+	@Test
+	public void testStream3() throws IOException{
+		
+		Path path = new File(this.getClass().getResource("stream-data.txt").getFile()).toPath();
+		
+		List<String> sorted = Files.lines( path )
+				.filter( s -> s.startsWith("a") )
+				.sorted()
+				.collect( Collectors.toList() );
+		
+		System.out.println( sorted.toString() );
+		
+	}
 	
 }
